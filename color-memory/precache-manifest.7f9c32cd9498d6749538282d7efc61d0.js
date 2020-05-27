4b6857db27224076bad1de29397f342f14c14251
Alexander <alexander@iMac-Alexander.local>
2020-05-27 17:05:21 +0300

    feat: add color-memory game

---
+++ color-memory/precache-manifest.7f9c32cd9498d6749538282d7efc61d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f6eac7af0047f39d8efd7fe5662d56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f976ee174c10d44b887",
+    "url": "/static/css/main.85152464.chunk.css"
+  },
+  {
+    "revision": "4cceb782c028fc1bdd92",
+    "url": "/static/js/2.526e8cf2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.526e8cf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f976ee174c10d44b887",
+    "url": "/static/js/main.8de97321.chunk.js"
+  },
+  {
+    "revision": "cd394ca9211485380225",
+    "url": "/static/js/runtime-main.67611196.js"
+  }
+]);
